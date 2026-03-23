08aeab6362cba311a4bc09405ed42e299c0b7c04
TristanCherrier <tristancherrier@gmail.com>
2026-03-23 13:46:01 +0100

    amelioration du robot (camera et physiques)

--- Scripts/robot.gd
+++ Scripts/robot.gd
@@ -3,15 +3,27 @@ extends RigidBody3D
 @export
 var CamNode : Node3D
 
+var going : bool
+
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	going = false
 	if Input.is_action_pressed("Roll_Forward"):
 		apply_torque(-CamNode.global_basis.x * (angular_damp + 1))
+		going = true
 	if Input.is_action_pressed("Roll_Back"):
 		apply_torque(CamNode.global_basis.x * (angular_damp + 1))
+		going = true
 	if Input.is_action_pressed("Roll_Left"):
 		apply_torque(CamNode.global_basis.z * (angular_damp + 1))
+		going = true
 	if Input.is_action_pressed("Roll_Right"):
 		apply_torque(-CamNode.global_basis.z * (angular_damp + 1))
+		going = true
+		
+	if going:
+		lock_rotation = false
+	else:
+		lock_rotation = true
