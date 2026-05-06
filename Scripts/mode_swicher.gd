8a01b403a58c6d11c69d272c9ac2b54af9f65ba8
HugoM <62258837+HugoMercey@users.noreply.github.com>
2026-05-06 17:19:34 +0200

    animation du monstre

--- Scripts/mode_swicher.gd
+++ Scripts/mode_swicher.gd
@@ -13,15 +13,15 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("Mode_Switch"):
 		mode = (mode + 1) % Mode_Nodes.size()
 		for i in range(Mode_Nodes.size()):
 			var node : Node = Mode_Nodes[i]
 			node.process_mode = PROCESS_MODE_DISABLED
-			var camera : Camera3D = node.find_child("*cam")
-			camera.current = false
-		var node : Node = Mode_Nodes[mode]
-		node.process_mode = PROCESS_MODE_ALWAYS
-		var camera : Camera3D = node.find_child("*cam")
+			var cam : Camera3D = node.find_child("*cam")
+			cam.current = false
+		var modeNode : Node = Mode_Nodes[mode]
+		modeNode.process_mode = PROCESS_MODE_ALWAYS
+		var camera : Camera3D = modeNode.find_child("*cam")
 		camera.current = true
