086cbec0ab6350443c589f8268003bff71c70b1f
TristanCherrier <tristancherrier@gmail.com>
2026-05-19 00:19:39 +0200

    fullscreen? fullscreen. (et alt+entree pour changer)

--- Autoload/Utils.gd
+++ Autoload/Utils.gd
@@ -1,9 +1,19 @@
 extends Node
 
+var last_window_mode = DisplayServer.WINDOW_MODE_WINDOWED
+
 func schedule(object, function_name, delay):
-  var timer = Timer.new()
-  timer.connect("timeout", Callable(object, function_name))
-  timer.connect("timeout", Callable(timer, "queue_free"))
-  timer.wait_time = delay
-  get_tree().root.add_child.call_deferred(timer)
-  timer.autostart = true
+	var timer = Timer.new()
+	timer.connect("timeout", Callable(object, function_name))
+	timer.connect("timeout", Callable(timer, "queue_free"))
+	timer.wait_time = delay
+	get_tree().root.add_child.call_deferred(timer)
+	timer.autostart = true
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("FullScreen"):
+		if get_window().mode == Window.MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(last_window_mode)
+		else:
+			last_window_mode = get_window().mode
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN) 
